962f76244d54f922375095b913d36ed1847cda81
Carlos Bellino <cbellino@lari.digital>
2018-03-02 16:41:26 +0100

    script shortcuts package added as dependecy of aeon, moved packages to custom packages/folder

---
+++ packages/custom/script.skinshortcuts/package.mk
@@ -0,0 +1,23 @@
+PKG_NAME="script.skinshortcuts"
+PKG_VERSION="1.0.17"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/BigNoid/script.skinshortcuts"
+PKG_URL="https://mirrors.kodi.tv/addons/krypton/script.skinshortcuts/$PKG_NAME-$PKG_VERSION.zip"
+PKG_SOURCE_DIR="$PKG_NAME"
+PKG_DEPENDS_TARGET="toolchain kodi"
+PKG_SECTION="custom"
+PKG_SHORTDESC="Makes user customizable shortcuts on the home page easier for skinners."
+PKG_LONGDESC="Makes user customizable shortcuts on the home page easier for skinners."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+  :
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/share/kodi/addons/script.skinshortcuts
+    cp -a */ $INSTALL/usr/share/kodi/addons/script.skinshortcuts
+}
