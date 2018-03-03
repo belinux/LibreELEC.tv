a4bc9d352b5c4cd4d82c97f4def77e050f6865e2
Carlos Bellino <carlosbellino@gmail.com>
2018-03-03 17:59:04 +0100

    connman fixed ip default, avoid wirzaed

---
+++ packages/custom/default.settings/package.mk
@@ -0,0 +1,26 @@
+PKG_NAME="default.settings"
+PKG_VERSION="0.0.1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE=""
+PKG_URL=""
+PKG_SOURCE_DIR=""
+PKG_DEPENDS_TARGET="toolchain kodi"
+PKG_SECTION="custom"
+PKG_SHORTDESC="Set of default settings to custom build"
+PKG_LONGDESC="Set of default settings to custom build"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+  :
+}
+
+makeinstall_target() {
+
+  # Avoid Wizard first run
+  mkdir -p $INSTALL/usr/share/kodi/config
+  cp -R $PKG_DIR/config $INSTALL/usr/share/kodi/config
+
+}
--- packages/mediacenter/kodi-theme-AeonNox/package.mk
+++ packages/mediacenter/kodi-theme-AeonNox/package.mk
@@ -6,7 +6,7 @@ PKG_SITE="https://github.com/BigNoid/Aeon-Nox"
 PKG_URL="https://mirrors.kodi.tv/addons/krypton/$PKG_NAME/$PKG_NAME-$PKG_VERSION.zip"
 PKG_SOURCE_DIR="$PKG_NAME"
 PKG_DEPENDS_TARGET="toolchain kodi script.grab.fanart script.skinshortcuts service.library.data.provider \
-                    resource.images.studios.white resource.images.recordlabels.white service.transmission"
+                    resource.images.studios.white resource.images.recordlabels.white default.settings"
 PKG_SECTION="mediacenter"
 PKG_SHORTDESC="kodi-theme-AeonNox: A skin for XBMC/Kodi"
 PKG_LONGDESC="kodi-theme-AeonNox: A skin for XBMC/Kodi."
@@ -22,7 +22,7 @@ makeinstall_target() {
   rm -f resources/screenshot-*.jpg
 
   mkdir -p $INSTALL/usr/share/kodi/config/skin.aeon.nox.5
-  cp -R $PKG_DIR/config/* INSTALL/usr/share/kodi/config/skin.aeon.nox.5/
+  cp -a $PKG_DIR/config/* $INSTALL/usr/share/kodi/config/skin.aeon.nox.5
 
   mkdir -p $INSTALL/usr/share/kodi/addons/skin.aeon.nox.5
   cp -a ./* $INSTALL/usr/share/kodi/addons/skin.aeon.nox.5
