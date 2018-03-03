b3d85612876c796beeb43d364ad18d72f5bbc92f
Carlos Bellino <carlosbellino@gmail.com>
2018-03-03 16:16:42 +0100

    custom config settings on kodi-config

--- packages/mediacenter/kodi-theme-AeonNox/package.mk
+++ packages/mediacenter/kodi-theme-AeonNox/package.mk
@@ -5,7 +5,8 @@ PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/BigNoid/Aeon-Nox"
 PKG_URL="https://mirrors.kodi.tv/addons/krypton/$PKG_NAME/$PKG_NAME-$PKG_VERSION.zip"
 PKG_SOURCE_DIR="$PKG_NAME"
-PKG_DEPENDS_TARGET="toolchain kodi script.grab.fanart script.skinshortcuts service.library.data.provider resource.images.studios.white resource.images.recordlabels.white service.transmission"
+PKG_DEPENDS_TARGET="toolchain kodi script.grab.fanart script.skinshortcuts service.library.data.provider \
+                    resource.images.studios.white resource.images.recordlabels.white service.transmission"
 PKG_SECTION="mediacenter"
 PKG_SHORTDESC="kodi-theme-AeonNox: A skin for XBMC/Kodi"
 PKG_LONGDESC="kodi-theme-AeonNox: A skin for XBMC/Kodi."
@@ -19,7 +20,12 @@ make_target() {
 
 makeinstall_target() {
   rm -f resources/screenshot-*.jpg
-  mkdir -p $INSTALL/usr/share/kodi/addons/
+
+  mkdir -p $INSTALL/usr/share/kodi/config/skin.aeon.nox.5
+  cp -R $PKG_DIR/config/* INSTALL/usr/share/kodi/config/skin.aeon.nox.5/
+
+  mkdir -p $INSTALL/usr/share/kodi/addons/skin.aeon.nox.5
   cp -a ./* $INSTALL/usr/share/kodi/addons/skin.aeon.nox.5
-  cp -R $PKG_DIR/config/script-skinshortcuts-includes.xml $INSTALL/usr/share/kodi/addons/skin.aeon.nox.5/1080i/
+  ln -sf /storage/.kodi/userdata/addon_data/skin.aeon.nox.5/script-skinshortcuts-includes.xml \
+         $INSTALL/usr/share/kodi/addons/skin.aeon.nox.5/1080i/script-skinshortcuts-includes.xml
 }
